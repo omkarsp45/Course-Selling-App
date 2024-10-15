ed2662fa094b8c7a29c3308252b1f6ad54b8ca65
Omkar Patil <omkar.spatil@outlook.com>
2024-10-16 00:59:33 +0530

    add couple of routes

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,58 @@
 const express = require('express');
 const userRouter = express.Router();
 const userMiddleware = require('../middlewares/userMiddleware.js');
+const bcrypt = require('bcrypt');
+const saltrounds = 10;
+const jwt = require('jsonwebtoken');
+const schema = require('../zod.js');
+const { userModel } = require('../db');
 
-userRouter.post('/sign-up', (req, res)=>{
-    res.json({
-        message: "Successfully Signed Up"
-    })
+userRouter.post('/sign-up', async (req, res) => {
+    try {
+        const { email, password, firstName, lastName } = schema.parse(req.body);
+        const salt = await bcrypt.genSalt(saltrounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        await userModel.create({ email, password: hashedPassword, firstName, lastName });
+        res.json({
+            message: "Signed Up Successfully"
+        });
+    } catch {
+        res.status(400).json({
+            message: "Credentials are not valid"
+        });
+    }
 });
 
-userRouter.post('/login', (req, res)=>{
-    res.json({
-        message: "Successfully Logged In"
-    });
+userRouter.post('/login', async (req, res) => {
+    try {
+        const { email, password } = schema.parse(req.body);
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "Email Not Found"
+            });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(400).json({
+                message: "Incorrect Password"
+            });
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_USER_PASSWORD, { expiresIn: '1d' });
+        res.json({
+            message: "Logged In Successfully",
+            token
+        });
+    } catch {
+        res.status(400).json({
+            message: "Credentials are not valid"
+        });
+    }
 });
 
-userRouter.get('/my-courses', (req, res)=>{
+userRouter.get('/my-courses', (req, res) => {
     res.json({
-        message: "This are your courses"
+        message: "All Courses"
     });
 });
 
