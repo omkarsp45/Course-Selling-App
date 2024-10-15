188b0f42220fbb7dba8005bf76430b40830b5ffd
Omkar Patil <omkar.spatil@outlook.com>
2024-10-16 01:32:10 +0530

    update zod.js

--- zod.js
+++ zod.js
@@ -1,10 +1,15 @@
 const zod = require('zod');
 
-const schema = zod.object({
+const signUpSchema = zod.object({
     email: zod.string().email().min(5),
     password: zod.string().min(8).max(20),
     firstName: zod.string().min(3),
     lastName: zod.string().min(3)
 });
 
-module.exports = schema
+const loginSchema = zod.object({
+    email: zod.string().email().min(5),
+    password: zod.string().min(8).max(20)
+});
+
+module.exports = { signUpSchema, loginSchema }
