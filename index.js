46ad903dde084a96b9012e5548294830d38ce06d
Omkar Patil <omkar.spatil@outlook.com>
2024-10-21 10:19:16 +0530

    add rate-limit middleware

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
+const rateLimit = require("express-rate-limit");
 const userRouter = require("./routes/user");
 const courseRouter = require("./routes/course");
 const adminRouter = require("./routes/admin");
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10,
+    handler: (req, res) => {
+        res.status(429).json({
+            success: false,
+            message: "You have sent too many requests. Please try again later.",
+            status: 429
+        });
+    }
+});
+app.use(limiter);
 app.use(express.json());
 
 app.use('/user', userRouter);
